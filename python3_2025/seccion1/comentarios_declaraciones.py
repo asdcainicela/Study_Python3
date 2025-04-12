dda8fea6db9576fdae4d9549132fb9952eaaa2a9
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-11 23:01:10 -0500

    Add comentarios_declaraciones.py to demonstrate cat attributes and printing

---
+++ python3_2025/seccion1/comentarios_declaraciones.py
@@ -0,0 +1,18 @@
+#Gatos
+nombre = "Thor"
+edad = 1
+raza = "Naranja"
+sexo = "Macho"
+
+print("El nombre del gato es: ", nombre)
+print("La edad del gato es: ", edad)
+print("La raza del gato es: ", raza)
+print("El sexo del gato es: ", sexo)
+
+#Gatos
+nombre2= "Patitas"
+edad2 = 1
+raza2 = "siames"
+sexo2 = "Hembra"
+
+print("La gata es: ", nombre2, "y sus datos son: ", edad2, raza2, sexo2)

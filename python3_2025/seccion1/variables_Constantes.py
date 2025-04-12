307ba34ca548c37ac8041ef31fa12cc23d10c6a9
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-11 23:12:34 -0500

    Add Constantes.py and variables_Constantes.py to demonstrate constant usage and variable assignments

---
+++ python3_2025/seccion1/variables_Constantes.py
@@ -0,0 +1,18 @@
+import Constantes 
+
+# variables
+variable = True
+print(variable)
+
+variable1 = variable2 = variable3 = "Gatos"
+print(variable1, variable2, variable3)
+
+# asignacion multiple, pero mejor es veritcal
+name1, name2, name3 = "thor", "odin", "loki" 
+print(name1, name2, name3)
+
+
+# constantes
+# en este caso es una constante creada en otro archivo
+print(Constantes.PI)
+ 

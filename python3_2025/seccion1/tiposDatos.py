6b5c574b499c63af969dc9462607a07f12eda34a
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 15:57:09 -0500

    Add tiposDatos.py to demonstrate data types in Python

---
+++ python3_2025/seccion1/tiposDatos.py
@@ -0,0 +1,25 @@
+# Recordatorio de tipye of datos en Python
+# Tipos de datos en Python
+
+cadena = "hola xd"
+print(cadena) 
+print(type(cadena))
+
+iCadena = input("Introduce una cadena: ")
+print(iCadena)
+print(iCadena[0])
+print(iCadena[1])
+print(iCadena[-1])
+print(type(cadena))
+
+iMoney = input("Introduce una cantidad de dinero: ")
+print(iMoney)
+print(type(iMoney))
+
+iNumber = int(input("Introduce un número: "))
+print(iNumber)
+print(type(iNumber))
+
+iDecimal = float(input("Introduce un número decimal: "))    
+print(iDecimal)
+print(type(iDecimal))

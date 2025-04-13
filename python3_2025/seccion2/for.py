4c657bc01ec5fc3745ef88ba1789e09e67ddca8f
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-12 23:56:23 -0500

    Add for.py to demonstrate iteration over lists and calculating squares of numbers

---
+++ python3_2025/seccion2/for.py
@@ -0,0 +1,14 @@
+frutas = ["manzana", "banana", "naranja", "kiwi", "mango"]
+contador = 0
+ 
+for fruta in frutas:
+    contador += 1
+    print(f"Fruta {contador}: {fruta}")
+
+    # other ex
+
+    numeros = [1, 2, 3, 4, 5]
+
+    for numero in numeros:
+        cuadrado = numero ** 2
+        print(f"El cuadrado de {numero} es {cuadrado}")

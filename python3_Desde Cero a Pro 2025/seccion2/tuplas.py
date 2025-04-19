a4e608bc5dc58dd0fbc59e3f7a080b5f288e0f96
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-19 13:31:10 -0500

    Add tuplas.py and content

---
+++ python3_Desde Cero a Pro 2025/seccion2/tuplas.py
@@ -0,0 +1,26 @@
+#Una tupla en Python es una colección ordenada e inmutable de elementos.
+#  Es como una lista, pero no se puede modificar 
+# (ni agregar, ni eliminar, ni cambiar elementos) después de que se crea.
+
+"""
+tupla_uno = (5,)  # ✅ Esto es una tupla
+no_tupla = (5)    # ❌ Esto es solo el número 5
+"""
+
+#tupla con números enteros
+mi_tupla1 = (1,2,3)
+
+#mi tupla con cadenas de texto
+
+mi_tupla2= ("manzana", "babana", "cereza")
+
+#tupla mixta con diferentes tipos de datos
+mi_tupla3 = (1, "hola", 3.141)
+
+#tupla vacia
+
+tupla_vacia= ()
+
+print(mi_tupla1[0])
+print(mi_tupla2[1])
+print(mi_tupla3[2])

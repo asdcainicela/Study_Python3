7f4e0276422c4b735ef8694a3f35aa15932854e2
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-20 23:42:44 -0500

    Add diccionarios.py

---
+++ python3_Desde Cero a Pro 2025/seccion3/diccionarios.py
@@ -0,0 +1,18 @@
+persona = {
+    "nombre" : "Juan",
+    "edad": 30,
+    "ciudad" : "Madrid"
+}
+
+"""
+print(persona)
+for person in persona:
+    print(person)
+"""
+
+perfil=persona
+
+pCiudad=perfil["ciudad"]
+#pCiudad_1 = perfil[0] esto no
+
+print(pCiudad)

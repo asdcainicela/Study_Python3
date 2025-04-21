08d8b227aa49b1b2ab19ea2068109fe869d6ab48
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-20 23:25:29 -0500

    Add ejemplo_listas.py

---
+++ python3_Desde Cero a Pro 2025/seccion3/ejemplo_listas.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio: Lista de Colores
+Escribe un programa en Python que cree una lista de colores y luego muestre cada color en una línea separada.
+    Instrucciones:
+        1 Crea una lista llamada colores que contenga varios nombres de colores (por ejemplo, "rojo", "verde", "azul", etc.).
+        2 Utiliza un bucle for para recorrer la lista de colores.
+        3 En cada iteración del bucle, muestra un color en una línea separada utilizando la función print().
+"""
+colores = ["rojo", "verde", "azul", "morado", "negro", "blanco"]
+
+for color in colores:
+    print(color)
+
+# muy easy xd
+

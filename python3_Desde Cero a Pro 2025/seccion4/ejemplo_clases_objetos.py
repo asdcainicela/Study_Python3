535c61229367e7dfc1a421b7d3604df3f77df897
Gerald C.A. <gerald.cainicela.a@gmail.com>
2025-04-22 21:25:17 -0500

    Add example 2

--- python3_Desde Cero a Pro 2025/seccion4/ejemplo_clases_objetos.py
+++ python3_Desde Cero a Pro 2025/seccion4/ejemplo_clases_objetos.py
@@ -1,5 +1,4 @@
- 
-    
+#--------------------- Ejemplo 1   
 class calculadora:
     def __init__(self, numero):
         self.resultado = numero
@@ -26,4 +25,34 @@ calculo.multiplicar(4)
 calculo.restar(10)
 calculo.division(2)
 resultado = calculo.resultado
-print(resultado)
+print("Resultado: ", resultado)
+
+#--------------------- Ejemplo 2   
+class calculadora2:
+    def suma(self, num1, num2):
+        return num1 + num2
+    
+    def resta(self, num1, num2):
+        return num1 - num2
+    
+    def multiplicar(self, num1, num2):
+        return num1 * num2
+    
+    def divicion(self, num1, num2):
+        if num2 == 0:
+            return "Error: No se puede dividir por cero."
+        
+num1 = float(input("Ingresa el primer numero:"))
+num2 = float(input("Ingresa el segundo numero:"))
+
+calc = calculadora2()
+
+resulsuma = calc.suma(num1, num2)
+resulresta = calc.resta(num1, num2)
+resulmultiplicacion = calc.multiplicar(num1, num2)
+resuldivicion = calc.divicion(num1, num2)
+
+print("Suma:", resulsuma)
+print("Resta:", resulresta)
+print("Multiplicacion:", resulmultiplicacion)
+print("Divicion:", resuldivicion)

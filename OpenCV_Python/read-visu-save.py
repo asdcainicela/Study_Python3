bef91570e01b45afd4bfb97553ca6465c6b441a9
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-05-30 16:14:02 -0500

    Add image display and save functionality using OpenCV

---
+++ OpenCV_Python/read-visu-save.py
@@ -0,0 +1,8 @@
+import cv2
+
+imagen = cv2.imread('OpenCv_Python/img/img_zero.jpg', 1) # buscar imagen
+cv2.imshow("Prueba de imagen ", imagen) # mostrar imagen
+cv2.imwrite('OpenCv_Python/img/copy_img_zero.jpg', imagen) # save imagen
+cv2.waitKey(0)  # tiempo de espera
+cv2.destroyAllWindows() # cerrar window
+# Nota: Asegúrate de que la ruta de la imagen sea correcta y que la imagen exista en esa ubicación.
